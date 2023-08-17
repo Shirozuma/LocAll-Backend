3b9ac3d2f5c9261bd4e6a6cb5f64e4c1dbe01b0a
Sohini Mukhopadhyay <43903292+Shirozuma@users.noreply.github.com>
2023-08-17 09:46:24 +0530

    Delete LocAllApp/LocAllApp-master directory

--- LocAllApp/LocAllApp-master/src/main/java/com/localapp/Repository/PincodeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.localapp.Repository;
-
-import com.localapp.Model.Pincode;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository("pincodeRepository")
-public interface PincodeRepository extends JpaRepository<Pincode, Integer> {
-}
